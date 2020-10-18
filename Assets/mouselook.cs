861cf200929cb405b5e93c15cd1435c69340b0e6
OliveiraGi2 <giovannazinha2@gmail.com>
2020-10-18 15:50:19 -0300

    Gravidade e Pulo

--- Assets/mouselook.cs
+++ Assets/mouselook.cs
@@ -26,6 +26,7 @@ public class mouselook : MonoBehaviour
 
         xRotation -= mouseY; 
         xRotation = Mathf.Clamp (xRotation,-90f,90f); 
+        
         transform.localRotation = Quaternion.Euler(xRotation, 0f,0f);
     }
 }
