9f23f580427d1afc1a4ecdc3773c73a7097e87a5
OliveiraGi2 <giovannazinha2@gmail.com>
2021-01-12 22:00:10 -0300

    Interação com o Google VR

---
+++ Assets/endoflevel.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class endoflevel : MonoBehaviour
+{
+ 
+
+    public void endofgame ()
+    {
+       // Debug.Log("HIT");
+       //Application.Quit();
+       SceneManager.LoadScene("fps");
+    }
+}
